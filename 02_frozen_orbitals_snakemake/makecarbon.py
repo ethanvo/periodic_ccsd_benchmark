2a9411df02fb8e6b51a16c53b04f1fce4eb3e151
Ethan Vo <ethan.vo@hotmail.com>
2022-05-06 12:50:08 -0400

    make source files

--- 02_frozen_orbitals_snakemake/makecarbon.py
+++ 02_frozen_orbitals_snakemake/makecarbon.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from pyscf.pbc import gto
+from pyscf.pbc.tools import lattice, pyscf_ase
 from pyscf.gto.basis import parse_nwchem
 import json
 
@@ -7,15 +9,10 @@ def dump(data, filename):
     with open(filename, 'w') as fout:
         fout.write(json.dumps(data, indent=4))
 
-def get_basis(foru
-
-def write_basis(formula, material_components, basis_file):
-    basis = '{'
-    for element_index, element in enumerate(material_components[formula]):
-        if element_index > 0:
-            basis = basis + ', '
-        basis = basis + '\'' + element + '\': parse_nwchem.load(\'' + basis_file + '\', \'' + element + '\')'
-    basis = basis + '}'
+def get_basis(formula, material_components, basis_file):
+    basis = {}
+    for element in material_components[formula]:
+        basis[element] = parse_nwchem.load(basis_file, element)
     return basis
 
 def calculate_norbs(formula, basis):
@@ -34,6 +31,7 @@ def calculate_norbs(formula, basis):
     nmo = cell.nao
     nocc = cell.nelectron // 2
     nvir = nmo - nocc
+    return nmo, nocc, nvir
 
 material_components = {
         'c':['C'], 
@@ -109,18 +107,24 @@ ccpvdz = '/burg/berkelbach/users/eav2136/builds/ccgto/basis/gth-hf-rev/cc-pvdz-l
 ccpvtz = '/burg/berkelbach/users/eav2136/builds/ccgto/basis/gth-hf-rev/cc-pvdz-lc.dat'
 ccpvqz = '/burg/berkelbach/users/eav2136/builds/ccgto/basis/gth-hf-rev/cc-pvdz-lc.dat'
 
-basis_sets = [ccpvdz, ccpvtz, ccpvqz]
+basis_sets = {'ccpvdz': '/burg/berkelbach/users/eav2136/builds/ccgto/basis/gth-hf-rev/cc-pvdz-lc.dat', 'ccpvtz':'/burg/berkelbach/users/eav2136/builds/ccgto/basis/gth-hf-rev/cc-pvtz-lc.dat', 'ccpvqz':'/burg/berkelbach/users/eav2136/builds/ccgto/basis/gth-hf-rev/cc-pvqz-lc.dat'}
 
 materials = ['c', 'si', 'sic', 'bn', 'bp', 'aln', 'alp', 'mgo', 'mgs', 'lih', 'lif', 'licl']
 
 for formula in materials:
-    for basis in basis_sets:
-        
-
-material = dict([
-('formula', 'c'),
-('basis', {'C': parse_nwchem.load('/burg/berkelbach/users/eav2136/builds/ccgto/basis/gth-hf-rev/cc-pvdz-lc.dat', 'C')})
-])
-filename = 'data/c_cc-pvdz-lc.json'
-dump(material, filename)
-
+    for basis_name in basis_sets:
+        basis = get_basis(formula, material_components, basis_sets[basis_name])
+        nmo, nocc, nvir = calculate_norbs(formula, basis)
+        material = dict([
+            ('formula', formula),
+            ('basis', basis),
+            ('vb_scaled_center', vb_scaled_centers[formula]),
+            ('vb_nroots', vb_nroots[formula]),
+            ('cb_scaled_center', cb_scaled_centers[formula]),
+            ('cb_nroots', cb_nroots[formula]),
+            ('nmo', nmo),
+            ('nocc', nocc),
+            ('nvir', nvir)
+            ])
+        filename = 'data/' + formula + '_' + basis_name + '.json'
+        dump(material, filename)
